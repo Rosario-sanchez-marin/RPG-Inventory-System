0ab2cb908f153f28155542225661e4b5a2b44670
virgen-juarez-camila <virgenjuarezcamilam3s2@gmail.com>
2026-04-29 16:09:51 -0600

    Update playerRepository.js

--- project-root/src/queries/playerRepository.js
+++ project-root/src/queries/playerRepository.js
@@ -1,1 +1,15 @@
+// Function to get a player's level without their ID
+async function getPlayerLevel(playerName) {
+  const query = { "playerName": playerName };
+  const projection = { "level": 1, "_id": 0 };
 
+  // This is where you put the logic
+  return await db.collection('players').findOne(query, { projection });
+}
+
+// Function to find players with a specific item type
+async function getPlayersByItemType(itemType) {
+  const query = { "inventory.type": itemType };
+  
+  return await db.collection('players').find(query).toArray();
+}
