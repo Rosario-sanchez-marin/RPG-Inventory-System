c99b7769e3f749b7161389718eb62d75c8a23554
virgen-juarez-camila <virgenjuarezcamilam3s2@gmail.com>
2026-04-29 16:07:51 -0600

    Update find_essential_items.js

--- project-root/src/queries/find_essential_items.js
+++ project-root/src/queries/find_essential_items.js
@@ -1,1 +1,14 @@
+// Database: rpg_game
+// Collection: players
+// Goal: Find high-level players and list their loot
 
+const highLevelQuery = { 
+    // YOUR FILTER HERE
+};
+
+const inventoryProjection = { 
+    // YOUR PROJECTION HERE
+};
+
+// Execution hint for MongoDB Shell
+db.players.find(highLevelQuery, inventoryProjection);
